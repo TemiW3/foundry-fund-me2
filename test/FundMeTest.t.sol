5a7b6e60acf16102c048d4b91b9f1d385ceedee1
Temi <temiw3@gmail.com>
2025-11-01 23:21:38 +0000

    done some test for fund me did some refactoring to use the helperconfig

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    // Write your test cases here
+    FundMe fundMe;
+
+    function setUp() external {
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployer = new DeployFundMe();
+        fundMe = deployer.run();
+    }
+
+    function testMinimumUsdIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+}
